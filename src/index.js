6fcd7634c2eb98733d83561a55706d2b44f854ae
Jessica <github email address>
2020-06-15 16:38:24 -0400

    updated users

---
+++ src/index.js
@@ -0,0 +1,127 @@
+document.addEventListener('DOMContentLoaded', function(e){
+
+// Users Cart
+    const usersCartUrl = "http://localhost:3000/users_carts"
+    const restaurantMenusUrl = "http://localhost:3000/restaurant_menus"
+    const itemsUrl = "http://localhost:3000/items"
+    const itemOl = document.querySelector('.item')
+
+    const fetchCartItems = () => {
+        fetch(`${usersCartUrl}/1`)
+        .then(resp => resp.json())
+        .then(cartItems => renderCartItems(cartItems))
+    };
+    
+    const renderCartItems = cartItems => {
+        cartItems.forEach(cartItem => {
+            renderCartItem(cartItem)
+        })
+    };
+
+    let total = 0
+
+    const renderCartItem = (cartItem) => {
+        const itemLi = document.createElement('li')
+        itemLi.dataset.id = cartItem.id
+        itemLi.className = "user"
+        const itemId = `${cartItem.item_id}`
+        const quantity = `${cartItem.quantity}`
+
+        fetch(`${itemsUrl}/${itemId}`)
+        .then(resp => resp.json())
+        .then(item => {
+            itemLi.innerHTML = `
+            ${item.name}            
+            <br/>
+            Price: $${item.price}
+            <br/>
+            Quantity: ${quantity}
+            `
+            itemOl.appendChild(itemLi)
+
+            let subtotal = quantity * parseInt(item.price)
+            total = total + subtotal 
+            const pTotal = document.querySelector('.cart-total')
+            pTotal.innerText = `Total: $${total}`
+        })
+
+    };
+
+    fetchCartItems()
+
+    /////End Users Cart
+
+
+
+
+
+    ////Don's code begins
+
+    fetch("http://localhost:3000/restaurant_menus")
+    .then(r => r.json())
+    .then(restaurants => restaurants.forEach(x => createRestaurant(x)))
+    
+    
+    restaurants = document.getElementById("restaurants")
+    function createRestaurant(restaurant) {
+        restaurantTag = document.createElement("div")
+        restaurantTag.dataset.id = restaurant.id
+        restaurantTag.className = "restaurant"
+        restaurantTag.innerHTML = ""
+        restaurantTag.innerHTML = `
+        <h2>${restaurant.name}</h2>
+        <h4>${restaurant.cuisine}</h4>
+        <p>${restaurant.address}</p>
+        <p>${restaurant.phone_number}</p>
+        <hr>
+        `
+        restaurants.append(restaurantTag)
+    }
+    restaurantMenu = document.getElementById("restaurantMenu")
+    
+    function showMenu(menu){
+        restaurantMenu.innerHTML = ""
+        menu.forEach(createItem)
+    }
+    
+    function createItem(item){
+        itemDiv = document.createElement("div")
+        itemDiv.dataset.id = item.id
+        itemDiv.innerHTML = `
+        <p><strong>${item.name}</strong> <em>$${item.price}</em></p>
+        <p>${item.description}</p>
+        <button class="addToCart">Add to Cart</button>
+        <hr>
+        `
+        restaurantMenu.append(itemDiv)
+    }
+    
+    document.addEventListener("click", function(e) {
+        if (e.target.className === "restaurant"){
+            let restaurantId = e.target.dataset.id
+            fetch(`http://localhost:3000/restaurant_menus/${restaurantId}`).then(r => r.json()).then(showMenu)
+        } else if (e.target.className === "addToCart") {
+            let itemId = e.target.parentNode.dataset.id
+            fetch(`http://localhost:3000/cart_items`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "accept": "application/json"
+                },
+                body: JSON.stringify({item_id: itemId, quantity: 1, users_cart_id: 1})
+            }).then(r => r.json()).then(console.log)
+        }
+    })
+    
+    
+    
+///don's code ends
+
+
+
+
+
+
+
+
+})
