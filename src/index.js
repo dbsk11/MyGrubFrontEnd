b8f77ca8305404e4b0652eab44d57c0d8a7abfc6
Jessica <github email address>
2020-06-15 14:35:01 -0400

    updated controller

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ document.addEventListener('DOMContentLoaded', function(e){
 
 
 // Jessica's code
-    usersCartUrl = "http://localhost:3000/users_carts"
+    const usersCartUrl = "http://localhost:3000/users_carts"
+    const restaurantMenusUrl = "http://localhost:3000/restaurant_menus"
+    const itemsUrl = "http://localhost:3000/items"
+
     
     const itemUl = document.querySelector('.item')
 
     const fetchCartItems = () => {
-        fetch(`${usersCartUrl}/3`)
+        fetch(`${usersCartUrl}/1`)
         .then(resp => resp.json())
         .then(cartItems => renderCartItems(cartItems))
     }
@@ -21,12 +24,16 @@ document.addEventListener('DOMContentLoaded', function(e){
     const renderCartItem = (cartItem) => {
         const itemLi = document.createElement('li')
         itemLi.dataset.id = cartItem.id
-
         const itemId = `${cartItem.item_id}`
-        const itemName = Item.find_by_id(itemId)
-        console.log(itemName)
+
+        fetch(itemsURL)
+        .then(resp => resp.json())
+        .then
+        console.log(itemId)
+        // const itemName = Item.find_by_id(itemId)
+
         // const itemPrice = 
-        // const itemQuantity = 
+        const itemQuantity = `${cartItem.quantity}` 
 
         itemLi.innerHTML = `
         name: ${cartItem.item_id}
@@ -78,7 +85,7 @@ document.addEventListener('DOMContentLoaded', function(e){
         itemDiv = document.createElement("div")
         itemDiv.dataset.id = item.id
         itemDiv.innerHTML = `
-        <p><strong>${item.name}</strong> <em>${item.price}</em></p>
+        <p><strong>${item.name}</strong> <em>$${item.price}</em></p>
         <p>${item.description}</p>
         <button class="addToCart">Add to Cart</button>
         <hr>
@@ -91,15 +98,14 @@ document.addEventListener('DOMContentLoaded', function(e){
             let restaurantId = e.target.dataset.id
             fetch(`http://localhost:3000/restaurant_menus/${restaurantId}`).then(r => r.json()).then(showMenu)
         } else if (e.target.className === "addToCart") {
-            console.log()
             let itemId = e.target.parentNode.dataset.id
             fetch(`http://localhost:3000/cart_items`, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
-                    accept: "application/json"
+                    "accept": "application/json"
                 },
-                body: JSON.stringify({item_id: itemId, quantity: 1, users_cart_id: 3})
+                body: JSON.stringify({item_id: itemId, quantity: 1, users_cart_id: 1})
             }).then(r => r.json()).then(console.log)
         }
     })
