fb58e8801f0271457d4ba8a19ec85136fb264598
Jessica <github email address>
2020-06-17 17:02:22 -0400

    updated css

--- src/index.js
+++ src/index.js
@@ -1,26 +1,27 @@
 document.addEventListener('DOMContentLoaded', function(e){
-// constants
-const usersCartUrl = "http://localhost:3000/users_carts"
-const cartItemUrl = "http://localhost:3000/cart_items"
-const restaurantMenusUrl = "http://localhost:3000/restaurant_menus"
-const itemsUrl = "http://localhost:3000/items"
-const itemOl = document.querySelector('.item')
-let total = 0
-let userCardId = 1
+    // constants
+    const usersCartUrl = "http://localhost:3000/users_carts"
+    const cartItemUrl = "http://localhost:3000/cart_items"
+    const restaurantMenusUrl = "http://localhost:3000/restaurant_menus"
+    const itemsUrl = "http://localhost:3000/items"
+    const itemOl = document.querySelector('.item')
+    const restaurants = document.getElementById("restaurants")
+    let total = 0
+    let userCardId = 1
 
 // UserCart - fetching usercart data
-const fetchCartItems = () => {
-fetch(`${usersCartUrl}/${userCardId}`)
-.then(resp => resp.json())
-.then(cartItems => renderCartItems(cartItems))
-};
+    const fetchCartItems = () => {
+        fetch(`${usersCartUrl}/${userCardId}`)
+        .then(resp => resp.json())
+        .then(cartItems => renderCartItems(cartItems))
+    };
 
 // UserCart - render all cart items
-const renderCartItems = cartItems => {
-cartItems.forEach(cartItem => {
-    renderCartItem(cartItem)
-})
-};
+    const renderCartItems = cartItems => {
+        cartItems.forEach(cartItem => {
+            renderCartItem(cartItem)
+        })
+    };
 
 // UserCart - render individual cart item
     const renderCartItem = (cartItem) => {
@@ -34,7 +35,7 @@ cartItems.forEach(cartItem => {
         .then(resp => resp.json())
         .then(item => {
             itemLi.innerHTML = `
-            <h4 class="item">${item.name} - <em>$$ ${item.price}</em></h4>
+            <h4 class="item">${item.name} - <em>$ ${item.price}</em></h4>
             <button class="Remove Item">Remove Item</button>
             `
             itemOl.appendChild(itemLi)
@@ -44,54 +45,35 @@ cartItems.forEach(cartItem => {
             const pTotal = document.querySelector('.cart-total')
             pTotal.innerText = `Total: $ ${total}`
         })
-
     };
 
 // RestaurantMenu - fetch restaurant data
-fetch(restaurantMenusUrl).then(r => r.json()).then(restaurants => restaurants.forEach(x => createRestaurant(x)));
+    fetch(restaurantMenusUrl).then(r => r.json()).then(restaurants => restaurants.forEach(x => createRestaurant(x)));
 
-let restaurantList = []
-let menuList = []
+    let restaurantList = []
+    let menuList = []
 
-// restaurantList.forEach(restro => {
-//     if (restro.cuisine === "Thai") {
-//         thaiCuisine.push(restro)
-//     } else if (restro.cuisine === "American") {
-//         americanCuisine.push(restro)
-//     } else if (restro === "Italian") {
-//         italianCuisine.push(restro)
-//     }
-// })
-// let americanCuisine = []
-// let thaiCuisine = []
-// let italianCuisine = []
-// console.log(restaurantList)
-// console.log(thaiCuisine)
-// RestaurantMenu - creating restaurant div and restaurant info
-const restaurants = document.getElementById("restaurants")
-
-function createRestaurant(restaurant) {
-    restaurantList.push(restaurant)
-    const restaurantTag = document.createElement("div")
-    restaurantTag.dataset.id = restaurant.id
-    restaurantTag.innerHTML = ""
-    restaurantTag.innerHTML = `
-    <h3 class="restaurant">${restaurant.name} - <em>${restaurant.cuisine}</em></h3>
-
-    <hr>
-    `
-    restaurants.append(restaurantTag)
-};
-
+//Restaurants - rendering restaurants
+    function createRestaurant(restaurant) {
+        restaurantList.push(restaurant)
+        const restaurantTag = document.createElement("div")
+        restaurantTag.dataset.id = restaurant.id
+        restaurantTag.innerHTML = ""
+        restaurantTag.innerHTML = `
+        <h3 class="restaurant">${restaurant.name} - <em>${restaurant.cuisine}</em></h3>
+        <hr>
+        `
+        restaurants.append(restaurantTag)
+    };
 
 // Items - rendering restaurant menu
-restaurantMenu = document.getElementById("restaurantItems")
-restaurantInfo = document.getElementById("restaurantInfo")
-function showMenu(menu){
-    restaurantMenu.innerHTML = ""
-    menu.forEach(item => menuList.push(item))
-    menu.forEach(createItem)
-};
+    restaurantMenu = document.getElementById("restaurantItems")
+    restaurantInfo = document.getElementById("restaurantInfo")
+    function showMenu(menu){
+        restaurantMenu.innerHTML = ""
+        menu.forEach(item => menuList.push(item))
+        menu.forEach(createItem)
+    };
 
 // Items - rendering individual items on restaurant menu
     function createItem(item){
@@ -117,102 +99,89 @@ function showMenu(menu){
     }
 
 //click eventlisteners
-//    let restaurantId = 1
-document.addEventListener("click", function(e) {
-if (e.target.className === "restaurant"){
-    restaurantId = e.target.parentNode.dataset.id
-    addRestaurantInfo(restaurantId)
+    document.addEventListener("click", function(e) {
+        if (e.target.className === "restaurant"){
+            restaurantId = e.target.parentNode.dataset.id
+            addRestaurantInfo(restaurantId)
+            fetch(`${restaurantMenusUrl}/${restaurantId}`).then(r => r.json()).then(showMenu)
+        } else if(e.target.className === "addToCart"){
+            const itemId = e.target.parentNode.dataset.id
+            fetch(cartItemUrl, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "accept": "application/json"
+                },
+                body: JSON.stringify({item_id: itemId, quantity: 1, users_cart_id: userCardId})
+            }).then(r => r.json()).then(renderCartItem)
+        } else if(e.target.className === "Remove Item"){
+            const cartItemId = e.target.parentNode.dataset.id
+            const parentNode = e.target.parentNode
 
-    fetch(`${restaurantMenusUrl}/${restaurantId}`).then(r => r.json()).then(showMenu)
-} else if(e.target.className === "addToCart"){
-    const itemId = e.target.parentNode.dataset.id
-    fetch(cartItemUrl, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "accept": "application/json"
-        },
-        body: JSON.stringify({item_id: itemId, quantity: 1, users_cart_id: userCardId})
-    }).then(r => r.json()).then(renderCartItem)
-} else if(e.target.className === "Remove Item"){
-    const cartItemId = e.target.parentNode.dataset.id
-    const parentNode = e.target.parentNode
+            const itemTag = parentNode.querySelector('.item')
+            const itemTagArray = itemTag.innerText.split(" ")
+            const priceIndex = itemTagArray.length - 1
+            const price = itemTagArray[priceIndex]
+            const pTotal = document.querySelector('.cart-total')
+            const subtotal = parseInt(pTotal.innerText.split(" ")[2])
+            let newTotal = subtotal - price
+            pTotal.innerText = `Total: $ ${newTotal}`       
 
-    const itemTag = parentNode.querySelector('.item')
-    const itemTagArray = itemTag.innerText.split(" ")
-    const priceIndex = itemTagArray.length -1
-    const price = itemTagArray[priceIndex]
-    const pTotal = document.querySelector('.cart-total')
-    const subtotal = parseInt(pTotal.innerText.split(" ")[2])
-    let newTotal = subtotal - price
-    pTotal.innerText = `Total: $ ${newTotal}`       
+            fetch(`${cartItemUrl}/${cartItemId}`, {
+                method: "DELETE"
+            })
+            .then(e.target.parentNode.remove())
+        } else if(e.target.className === "checkout"){
+            let cartList = document.querySelectorAll(".user")
+            fetch(`${usersCartUrl}/${userCardId}`).then(r => r.json()).then(data => data.forEach(
+                cartData => fetch(`${cartItemUrl}/${cartData.id}`, {
+                    method: "DELETE"
+                })
+            ))
+            cartList.forEach(item => item.remove())
+            total = 0
+            const pTotal = document.querySelector('.cart-total')
+            pTotal.innerText = `Total: $ 0`       
+        }
+    });
 
-    fetch(`${cartItemUrl}/${cartItemId}`, {
-        method: "DELETE"
+    const cuisineFilter = document.querySelector('.cuisine')
+    cuisineFilter.addEventListener("change", function(e){
+        if (e.target.value == "American"){
+            restaurants.innerHTML=""
+            fetch(restaurantMenusUrl).then(r => r.json()).then(data => data.filter(restro => restro.cuisine === "American")).then(restaurants => restaurants.forEach(x => createRestaurant(x)));
+        } else if (e.target.value === "Thai"){
+            restaurants.innerHTML=""
+            fetch(restaurantMenusUrl).then(r => r.json()).then(data => data.filter(restro => restro.cuisine === "Thai")).then(restaurants => restaurants.forEach(x => createRestaurant(x)));
+        } else if (e.target.value === "Italian"){
+            restaurants.innerHTML=""
+            fetch(restaurantMenusUrl).then(r => r.json()).then(data => data.filter(restro => restro.cuisine === "Italian")).then(restaurants => restaurants.forEach(x => createRestaurant(x)));
+        } else if (e.target.value === "All"){
+            restaurants.innerHTML=""
+            fetch(restaurantMenusUrl).then(r => r.json()).then(restaurants => restaurants.forEach(x => createRestaurant(x)));
+        }
     })
-    .then(e.target.parentNode.remove())
-} else if(e.target.className === "checkout"){
 
-    let cartList = document.querySelectorAll(".user")
+    const categoryFilter = document.querySelector('.category')
+    categoryFilter.addEventListener("change", function(e){
+        if (e.target.value === "Appetizer") {
+            restaurantMenu.innerHTML = ""
+            menuList.filter(item => item.category === "Appetizer").forEach(createItem)
+        } else if (e.target.value === "Entree") {
+            restaurantMenu.innerHTML = ""
+            menuList.filter(item => item.category === "Entree").forEach(createItem)
+        } else if (e.target.value === "Beverages") {
+            restaurantMenu.innerHTML = ""
+            menuList.filter(item => item.category === "Beverages").forEach(createItem)
+        } else if (e.target.value === "Dessert"){
+            restaurantMenu.innerHTML = ""
+            menuList.filter(item => item.category === "Dessert").forEach(createItem)
+        } else if (e.target.value === "All") {
+            restaurantMenu.innerHTML = ""
+            menuList.forEach(createItem)
+        }
+    })
 
-
-    fetch(`${usersCartUrl}/${userCardId}`).then(r => r.json()).then(data => data.forEach(
-        cartData => fetch(`${cartItemUrl}/${cartData.id}`, {
-            method: "DELETE"
-        })
-
-    ))
-    cartList.forEach(item => item.remove())
-    //cartList.remove()
-    total = 0
-    const pTotal = document.querySelector('.cart-total')
-    pTotal.innerText = `Total: $ 0`       
-}
-});
-
-const cuisineFilter = document.querySelector('.cuisine')
-cuisineFilter.addEventListener("change", function(e){
-if (e.target.value == "American"){
-    restaurants.innerHTML=""
-    fetch(restaurantMenusUrl).then(r => r.json()).then(data => data.filter(restro => restro.cuisine === "American")).then(restaurants => restaurants.forEach(x => createRestaurant(x)));
-
-
-} else if (e.target.value === "Thai"){
-    restaurants.innerHTML=""
-    fetch(restaurantMenusUrl).then(r => r.json()).then(data => data.filter(restro => restro.cuisine === "Thai")).then(restaurants => restaurants.forEach(x => createRestaurant(x)));
-
-} else if (e.target.value === "Italian"){
-    restaurants.innerHTML=""
-    fetch(restaurantMenusUrl).then(r => r.json()).then(data => data.filter(restro => restro.cuisine === "Italian")).then(restaurants => restaurants.forEach(x => createRestaurant(x)));
-} else if (e.target.value === "All"){
-    restaurants.innerHTML=""
-    fetch(restaurantMenusUrl).then(r => r.json()).then(restaurants => restaurants.forEach(x => createRestaurant(x)));
-}
-})
-
-const categoryFilter = document.querySelector('.category')
-categoryFilter.addEventListener("change", function(e){
-if (e.target.value === "Appetizer") {
-    restaurantMenu.innerHTML = ""
-    menuList.filter(item => item.category === "Appetizer").forEach(createItem)
-
-} else if (e.target.value === "Entree") {
-    restaurantMenu.innerHTML = ""
-    menuList.filter(item => item.category === "Entree").forEach(createItem)
-
-} else if (e.target.value === "Beverages") {
-    restaurantMenu.innerHTML = ""
-    menuList.filter(item => item.category === "Beverages").forEach(createItem)
-} else if (e.target.value === "Dessert"){
-    restaurantMenu.innerHTML = ""
-    menuList.filter(item => item.category === "Dessert").forEach(createItem)
-
-} else if (e.target.value === "All") {
-    restaurantMenu.innerHTML = ""
-    menuList.forEach(createItem)
-}
-})
-
-fetchCartItems();
+    fetchCartItems();
 
 });
